9f972d41799adb97a2a140b7f1387e1613ff1062
coehorn <coehorn88@gmail.com>
2021-10-06 10:49:43 +0200

    Basic Styling

--- polls_app/polls/admin.py
+++ polls_app/polls/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date', 
+        description = 'Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+admin.site.register(Question, QuestionAdmin)
+
+
